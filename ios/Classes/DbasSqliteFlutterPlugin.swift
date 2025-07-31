0874140e969f158db8b3c550362fd00ba4239f83
Rafael Gasperetti <rafaelgasperetti@dailysoftwaresystems.com>
2025-07-31 03:34:47 -0300

    Clean swift classes

--- ios/Classes/DbasSqliteFlutterPlugin.swift
+++ ios/Classes/DbasSqliteFlutterPlugin.swift
@@ -3,17 +3,6 @@ import UIKit
 
 public class DbasSqliteFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "dbas_sqlite_flutter", binaryMessenger: registrar.messenger())
-    let instance = DbasSqliteFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
-    }
   }
 }
