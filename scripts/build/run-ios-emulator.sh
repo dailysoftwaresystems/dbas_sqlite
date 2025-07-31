7158b02e015e61f075a9ebd99a196ff9cf1266be
Rafael Gasperetti <rafaelgasperetti@outlook.com>
2025-07-31 15:53:45 -0300

    Fix script to build with clean

--- scripts/build/run-ios-emulator.sh
+++ scripts/build/run-ios-emulator.sh
@@ -3,4 +3,8 @@ set -e
 
 cd "$(dirname "$0")/../../example"
 
+flutter clean
+rm -rf ios/Pods ios/Podfile.lock ios/Runner.xcworkspace
+flutter pub get
+pod install --project-directory=ios
 flutter run -d "iPhone 16 Plus" --project-root=example
