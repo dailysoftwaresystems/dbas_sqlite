eab476b1965aaf1e2b71ed866af14ec669b5eccf
Rafael Gasperetti <rafaelgasperetti@outlook.com>
2025-08-04 14:55:59 -0300

    Enahnce dbas_sqlite libs to be inside each platform

--- scripts/sqlite/sync_sqlite_lib.ps1
+++ scripts/sqlite/sync_sqlite_lib.ps1
@@ -51,10 +51,16 @@ Write-Host "Copying android binaries..."
 New-Item -ItemType Directory -Force -Path "$SCRIPT_DIR/../../android/src/main/jniLibs/arm64-v8a" | Out-Null
 New-Item -ItemType Directory -Force -Path "$SCRIPT_DIR/../../android/src/main/jniLibs/armeabi-v7a" | Out-Null
 New-Item -ItemType Directory -Force -Path "$SCRIPT_DIR/../../android/src/main/jniLibs/x86_64" | Out-Null
+New-Item -ItemType Directory -Force -Path "$SCRIPT_DIR/../../windows/libs" | Out-Null
+New-Item -ItemType Directory -Force -Path "$SCRIPT_DIR/../../linux/libs" | Out-Null
+New-Item -ItemType Directory -Force -Path "$SCRIPT_DIR/../../web/libs" | Out-Null
 
 Copy-Item "$OUT_DIR/android/a64/*" -Destination "$SCRIPT_DIR/../../android/src/main/jniLibs/arm64-v8a" -Recurse -Force
 Copy-Item "$OUT_DIR/android/armeabi/*" -Destination "$SCRIPT_DIR/../../android/src/main/jniLibs/armeabi-v7a" -Recurse -Force
 Copy-Item "$OUT_DIR/android/x86_64/*" -Destination "$SCRIPT_DIR/../../android/src/main/jniLibs/x86_64" -Recurse -Force
+Copy-Item "$OUT_DIR/windows/*" -Destination "$SCRIPT_DIR/../../windows/libs" -Recurse -Force
+Copy-Item "$OUT_DIR/linux/*" -Destination "$SCRIPT_DIR/../../linux/libs" -Recurse -Force
+Copy-Item "$OUT_DIR/web/*" -Destination "$SCRIPT_DIR/../../web/libs" -Recurse -Force
 
 Write-Host "Copying ios binaries..."
 Copy-Item "$OUT_DIR/ios/dbas_sqlite.xcframework" -Destination "$SCRIPT_DIR/../../ios" -Recurse -Force
