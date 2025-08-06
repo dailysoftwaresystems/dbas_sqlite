8aced08f7cdd26ba04b495b3e717ebcb98d528c8
Rafael Gasperetti <rafaelgasperetti@outlook.com>
2025-08-06 12:36:37 -0300

    WIP apple

--- scripts/run/macos.sh
+++ scripts/run/macos.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Fix UTF-8 encoding issues with CocoaPods
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 if [[ " $@ " =~ " --force-head " ]]; then
   echo "Forcing HEAD reset and pull"
   git reset --hard
